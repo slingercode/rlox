22d8d28d7befc45d90d0b59edf4f36751f6149b2
Noel Espino Córdova <github@ednoesco.com>
2022-08-28 19:47:16 -0500

    Rlox struct with error handling

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,14 @@ use cli::{Cli, Commands};
 
 fn main() {
     let cli_instance = Cli::parse();
+    let mut rlox_instance = rlox::Rlox { had_error: false };
 
     match cli_instance.command {
         Some(Commands::Run { path }) => {
-            rlox::run_file(path);
+            rlox_instance.run_file(path);
         }
         None => {
-            rlox::run_promt().unwrap();
+            rlox_instance.run_promt().unwrap();
         }
     }
 }
--- src/rlox.rs
+++ src/rlox.rs
@@ -1,37 +1,58 @@
-use std::{fs, io::{self, Write}};
+use std::{fs, io::{self, Write}, process};
 
-pub fn run_file(path: String) {
-    let content = fs::read_to_string(path)
-        .expect("You need to provide a valid source file");
-
-    run(content);
+pub struct Rlox {
+    pub had_error: bool,
 }
 
-pub fn run_promt() -> io::Result<()> {
-    let mut buf = String::new();
-    let end_word = String::from("exit");
+impl Rlox {
+    pub fn run_file(&self, path: String) {
+        let content = fs::read_to_string(path)
+            .expect("You need to provide a valid source file");
+        
+        self.run(content);
 
-    println!("rlox interpreter\n");
-
-    loop {
-        print!("> ");
-
-        io::stdout().flush()?;
-        io::stdin().read_line(&mut buf)?;
-
-        let input = buf.trim().to_string();
-
-        if input.eq(&end_word) {
-            break;
+        if self.had_error {
+            process::exit(0);
         }
-
-        run(input);
-        buf.clear();
     }
 
-    return Ok(());
-}
+    pub fn run_promt(&mut self) -> io::Result<()> {
+        let mut buf = String::new();
+        let end_word = String::from("exit");
+    
+        println!("rlox interpreter\n");
+    
+        loop {
+            print!("> ");
+    
+            io::stdout().flush()?;
+            io::stdin().read_line(&mut buf)?;
+    
+            let input = buf.trim().to_string();
+    
+            if input.eq(&end_word) {
+                break;
+            }
+    
+            self.run(input);
+            self.had_error = false;
 
-fn run(source: String) {
-    print!("{}", source);
+            buf.clear();
+        }
+    
+        return Ok(());
+    }
+
+    fn run(&self, source: String) {
+        println!("{}", source);
+    }
+
+    fn error(&mut self, line: u32, message: String) {
+        self.report(line, String::new(), message);
+    }
+
+    fn report(&mut self, line: u32, on: String, message: String) {
+        println!("[line {}] on {}: {}", line, on, message);
+        self.had_error = true;
+    }
 }
